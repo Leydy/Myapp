ec8773b328794a24c86b88321dcc61eb2b3baeec
leydy <leydymayumy@gmail.com>
2020-04-30 12:34:25 -0500

    pc

---
+++ app/src/main/java/com/example/myfirstproject/InterfaceToast.java
@@ -0,0 +1,5 @@
+package com.example.myfirstproject;
+
+public interface InterfaceToast {
+    void mensaje(String mensaje);
+}
---
+++ app/src/main/java/com/example/myfirstproject/Manipulacion.java
@@ -0,0 +1,49 @@
+package com.example.myfirstproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Manipulacion extends AppCompatActivity {
+    EditText edtnombre;
+    EditText edtmonto;
+    TextView txtresultado;
+    Button btnejecutar;
+    Button  btnmimodificar;
+    int centinela = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manipulacion);
+        edtnombre=findViewById(R.id.edtnombre);
+        edtmonto=findViewById(R.id.edtmonto);
+        txtresultado=findViewById(R.id.txtResultado);
+        btnejecutar=findViewById(R.id.btncambiar);
+        btnmimodificar=findViewById(R.id.btnmodificar);
+        btnejecutar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double cambio;
+                cambio=Double.parseDouble(edtmonto.getText().toString())*3.3;
+                txtresultado.setText(edtnombre.getText() + " el monto de "+ edtmonto.getText()+
+                        " dolares equivale a " + cambio.toString() + "Soles");
+            }
+        });
+    }
+
+    public void ocultar(View view) {
+        if(centinela == 0){
+            centinela = 1;
+            btnejecutar.setVisibility(View.GONE);
+            btnmimodificar.setText("MOSTRAR");
+        }
+        else {
+            centinela = 0;
+            btnejecutar.setVisibility(View.VISIBLE);
+        }
+        }
+    }
